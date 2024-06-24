5de26c1b40dab412d87c77c84d34a7679a01531b
tanveerprottoy <tanveer.prottoy@gmail.com>
2024-06-25 00:05:50 +0600

    add handler code

--- internal/api/delivery/http/route/file.go
+++ internal/api/delivery/http/route/file.go
@@ -6,6 +6,8 @@ import (
 	"github.com/tanveerprottoy/event-processor-go/internal/api/delivery/http/handler"
 )
 
-func File(basePattern string, handler *handler.File) {
-	http.HandleFunc("POST "+basePattern+"/upload", handler.Upload)
+func File(basePattern string, handler *handler.File) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.HandleFunc("POST "+basePattern+"/upload", handler.Upload)
+	})
 }
