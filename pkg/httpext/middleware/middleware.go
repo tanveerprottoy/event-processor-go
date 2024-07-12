cd42b731bb2b82ea52548440c10db384df6ec08c
tanveerprottoy <tanveer.prottoy@gmail.com>
2024-07-12 22:59:05 +0600

    rem not needed stuff

--- pkg/httpext/middleware/middleware.go
+++ pkg/httpext/middleware/middleware.go
@@ -13,7 +13,7 @@ func CORSEnableMiddleWare(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, X-CSRF-Token, company-id, project-id")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, X-CSRF-Token")
 		next.ServeHTTP(w, r)
 	})
 }
